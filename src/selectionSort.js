a9a224aaa97c61749338697d7ab11bb0d8a6485b
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-22 12:45:24 +0300

    added: selection sort interface

---
+++ src/selectionSort.js
@@ -0,0 +1,11 @@
+const arr = [2, 9, 5, 7, 8, 6, 4, 3, 1];
+
+const selectionSort = (arr) => {
+
+};
+
+/* testing */
+console.log('arr before selectionSort:');
+console.log(arr);
+console.log('arr before selectionSort:');
+console.log(selectionSort(arr));
