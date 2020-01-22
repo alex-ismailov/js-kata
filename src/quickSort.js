3c469843149713b4076a381ac550973c079b8cbe
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-22 11:28:34 +0300

    added: quickSort interface

---
+++ src/quickSort.js
@@ -0,0 +1,11 @@
+const arr = [2, 9, 5, 7, 8, 6, 4, 3, 1];
+
+const quickSort = (arr) => {
+
+};
+
+/* testing */
+console.log('arr before quickSort:');
+console.log(arr);
+console.log('arr before quickSort:');
+console.log(quickSort(arr));
