eb2b96daf9bfdc5f933b4631b1d0d83cd6124955
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-22 12:10:10 +0300

    added: bubbleSort(optimized) interface

---
+++ src/bubbleSort (optimized).js
@@ -0,0 +1,11 @@
+const arr = [2, 9, 5, 7, 8, 6, 4, 3, 1];
+
+const bubbleSort = (arr) => {
+
+};
+
+/* testing */
+console.log('arr before bubbleSort:');
+console.log(arr);
+console.log('arr before bubbleSort:');
+console.log(bubbleSort(arr));
